7787e15f5cc6c5d1f02bfb91e8220247820116f2
NosovSA76 <125302593+NosovSA76@users.noreply.github.com>
2023-11-11 06:08:06 +0200

    Finish

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,54 @@
+class Key {
+    private signature: number;
+  
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
+  class Person {
+    constructor(private key: Key) {
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
+  abstract class House {
+    protected door: boolean = false;
+    protected tenants: Person[] = [];
+  
+    constructor( protected key: Key) {
+    }
+  
+    abstract openDoor(key: Key): void;
+  
+    comeIn(person: Person): void {
+      if (this.door) {
+        this.tenants.push(person);
+        console.log(`${person.getKey().getSignature()} entered the house.`);
+      } else {
+        console.log('The door is closed.');
+      }
+    }
+  }
+  
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+        console.log('The door is now open.');
+      } else {
+        console.log('Invalid key. The door remains closed.');
+      }
+    }
+  }
+  
 const key = new Key();
 
 const house = new MyHouse(key);
